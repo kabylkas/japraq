943b71343f049993f1633d198c96e9d9665f5c25
Nursultan Kabylkas <kabylkas@gmail.com>
2020-10-17 17:03:08 -0700

    Summary: quick tests
    Adding quick tests in the main to check the functionality of the
    implemented methods.

--- src/dt.cpp
+++ src/dt.cpp
@@ -1,17 +1,33 @@
 #include <vector>
+#include <iostream>
 #include "tree_node.h"
+#include "question.h"
 #include "defines.h"
 
 int main() {
-    std::vector<int> v1 = {0,0,0,0,0};
-    std::vector<int> v2 = {1,0,1,0,1};
-    row_t r1;
-    r1.feature_vals = v1;
-    r1.label = 1;
-    row_t r2;
-    r2.feature_vals = v2;
-    r2.label = 0;
-    const row_ptrs_t rows = {&r1, &r2};
+    row_t r1, r2, r3, r4, r5;
+    r1 = {{1,1,1,1,1}, 1};
+    r2 = {{2,2,2,2,2}, 0};
+    r3 = {{1,2,1,4,1}, 1};
+    r4 = {{2,2,1,4,1}, 2};
+    r5 = {{1,2,1,1,1}, 0};
+
+    const row_ptrs_t rows = {&r1, &r2, &r3, &r4, &r5};
     tree_node tn = tree_node(rows);
     tn.print_rows();
+    std::cout << '\n';
+
+    question q(0, 1, feature_t::CATEGORICAL);
+    std::cout << q.eval(r1) << '\n';
+    std::cout << q.eval(r2) << '\n';
+
+    auto [rows1, rows2] = tn.partition(q);
+    tree_node tn1 = tree_node(rows1);
+    tree_node tn2 = tree_node(rows2);
+
+    tn1.print_rows();
+    std::cout << tn1.get_gini() << '\n';
+    tn2.print_rows();
+    std::cout << tn2.get_gini() << '\n';
+    
 }
