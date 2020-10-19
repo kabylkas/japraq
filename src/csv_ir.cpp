43833174e198a185980752d219c4d8cf39cd43bf
Nursultan Kabylkas <kabylkas@gmail.com>
2020-10-19 09:38:28 -0700

    Summary: starting implementation of csv parser
    The idea is to parse CSV file into the internal represantion for the
    decsion tree to interface quicker. This class will contain meta data,
    such column names and integer to string mappings of the categorical
    data.

---
+++ src/csv_ir.cpp
@@ -0,0 +1,27 @@
+#include "csv_ir.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+csv_ir::csv_ir(std::string csv_file_path) {
+    std::ifstream in_csv(csv_file_path);
+    if (!in_csv.is_open()) {
+        throw std::runtime_error("Failed to open the file");
+    }
+
+    // read first line: col names
+    std::string line, value;
+    if (in_csv.good()) {
+        std::getline(in_csv, line);
+        std::stringstream col_ss(line);
+        for (uint32_t i=0; std::getline(col_ss, value, ';'); ++i) {
+            this->feature_meta_data[i];
+            std::stringstream feat_ss(value);
+            std::string col_name, col_category;
+            std::getline(feat_ss, col_name, ':');
+            std::getline(feat_ss, col_category, ':');
+            std::cout << col_name << " " << col_category << '\n';
+        }
+    }
+}
---
+++ src/csv_ir.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <map>
+#include "defines.h"
+
+class csv_ir {
+    public:
+        csv_ir(std::string csv_file_path);
+
+    private:
+        std::map<uint32_t, feature_meta_t> feature_meta_data;
+        std::map<uint32_t, std::map<uint32_t, std::string>> int_to_categ; 
+        row_ptrs_t rows;
+
+};
