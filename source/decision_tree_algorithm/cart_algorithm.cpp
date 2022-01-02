a0dfd6c44719e2f3c37faf02aa62c68b7fbe3eda
Nursultan Kabylkas <kabylkas@gmail.com>
2022-01-02 03:09:13 -0600

    more implementation of cart algorithm

--- source/decision_tree_algorithm/cart_algorithm.cpp
+++ source/decision_tree_algorithm/cart_algorithm.cpp
@@ -8,8 +8,36 @@
 #include <deque>
 #include <memory>
 
+// Local libraries.
+#include "decision_tree_node.h"
+#include "question.h"
+
 namespace japraq
 {
+    // STATIC FUNCTIONS - BEGIN.
+    static double CalculateGini()
+    {
+
+    }
+
+    static bool Partition(const DecisionTreeDataset& dataset, const Question& question, std::vector<uint32_t>& true_indicies, std::vector<uint32_t> false_indicies, std::string& error_message)
+    {
+
+    }
+
+    static bool BestPartition
+    (
+        const DecisionTreeDataset& dataset,
+        const std::vector<uint32_t> node_indicies,
+        std::vector<uint32_t>& true_indicies,
+        std::vector<uint32_t> false_indicies,
+        std::string& error_message
+    )
+    {
+
+    }
+    // STATIC FUNCTIONS - END.
+
     bool CartAlgorighm::BuildTree(const DecisionTreeDataset& dataset, DecisionTreeNode& root_node, std::string& error_message)
     {
         bool ret = false;
@@ -19,12 +47,29 @@ namespace japraq
         while(!nodes_to_process.empty())
         {
             auto& current_node = nodes_to_process.front();
+
+            // Get datapoints of the current node.
+            std::vector<uint32_t> node_indicies;
+            std::vector<uint32_t> true_indicies;
+            std::vector<uint32_t> false_indicies;
+            bool is_retrieved = current_node->GetDatasetIndicies(node_indicies, error_message);
+
+            // Partition the datapoints.
+            if (is_retrieved)
+            {
+                bool is_partitioned = BestPartition(dataset, node_indicies, true_indicies, false_indicies, error_message);
+                if (is_partitioned)
+                {
+                    current_node->SetTrueChild(true_indicies, error_message);
+                    current_node->SetFalseChild(false_indicies, error_message);
+                }
+            }
         }
     }
 
     bool CartAlgorighm::Infer(const DecisionTreeDataset& dataset, const DecisionTreeNode& root_node, const DataPoint& data_point, std::string& result, std::string& error_message)
     {
-        error_message = "Function not implemented.";
+        error_message = "Error: Function not implemented.";
         return false;
     }
 }
