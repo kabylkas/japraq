f3e9fa5db722dd720bd7dcfaae77287dbbe5066b
Nursultan Kabylkas <kabylkas@gmail.com>
2022-01-07 12:16:35 -0600

    removing old test directory

--- build/prebuild-windows.bat
+++ build/prebuild-windows.bat
@@ -1,3 +1,7 @@
+:: Copyright (c) 2021 Kabylkas Labs.
+:: Licensed under the Apache License, Version 2.0.
+
+:: Generate Visual Studio 2022 Solution files.
 mkdir windows
 cd windows
 cmake -G "Visual Studio 17 2022" ../../
